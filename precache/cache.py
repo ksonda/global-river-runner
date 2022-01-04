3af828bd31b4fbe1c1ca43e6f695bf19f955c090
Benjamin Webb <benjamin.miller.webb@gmail.com>
2022-01-04 10:55:37 -0500

    Update cache.py with some CLI usage

--- precache/cache.py
+++ precache/cache.py
@@ -1,12 +1,17 @@
 import grequests
 import time
+import sys
 
-def main():
+def main(argv):
+    start = int(argv.pop(0))
+    stop = 2938143 if argv[0] == "-1" else int(argv.pop(0))
+    step = 50 if len(argv) == 0 else int(argv.pop())
+    print(start, stop, step)
     start_time = time.time()
-    send_request(1000, 1500)
+    send_request(start, stop, step)
     print(f"End Time: {time.time() - start_time}")
 
-def send_request(startindex, endindex, step=2):
+def send_request(startindex, endindex, step):
     step = min(endindex-startindex, step)
     urls = make_urls(startindex, step)
     rs = (grequests.get(u) for u in urls)
@@ -15,11 +20,15 @@ def send_request(startindex, endindex, step=2):
     print("%.3f |" % (time.time() - start_time))
 
     if (startindex < endindex):
-        send_request(startindex+step, endindex)
+        send_request(startindex+step, endindex, step)
     
 def make_urls(start, step):
-    base_url = "https://merit-nldi.internetofwater.app/processes/river-runner/execution?id={}"
+    base_url = "https://merit.internetofwater.app/processes/river-runner/execution?id={}"
     return [base_url.format(i) for i in range(start, start+step)]
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) < 3:
+        print('Usage: {} <start_ogc_fid> <end_ogc_fid> <group_size>'.format(sys.argv[0]))
+        sys.exit(1)
+    
+    main(sys.argv[1:])
